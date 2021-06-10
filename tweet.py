928cbf06c046264fe9a91e74fc10687e1e4ef8b3
pranjalkul10 <65441661+pranjalkul10@users.noreply.github.com>
2021-06-10 17:12:26 +0530

    Create tweet.py

---
+++ tweet.py
@@ -0,0 +1,30 @@
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("oNIL1OS98awVJAZbnbsoGRhGW", 
+    "0CRV5G2btlHJluDnZwcoMC8f0EUQis874N12wrMaxbXsEEBswQ")
+auth.set_access_token("1400465690755096587-RiMVYAeuFKoK6fVFNQz1xb1J2jioB3", 
+    "lgbBuyDSxKVutVX2S3Kq78oTXq4m6brwVVnwykXwbh2SW")
+
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+api.update_status("Hello Twitter")
+
+user = api.get_user("@PRANJAL58541898")
+for following in user.friends():
+    print(following.name)
+print("User details:")
+print(user.name)
+print(user.description)
+print(user.location)
